4ef43d185712b1421a2a6acb372ab0c2d85a1105
VS\shabala <artemizgame@gmail.com>
2024-07-16 13:43:42 +1000

    JWT sign check in

--- Program.cs
+++ Program.cs
@@ -1,7 +1,45 @@
+using GateWay.API.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var identityServerSettings = new IdentityServerSettings();
+builder.Configuration.GetSection("IdentityServerSettings").Bind(identityServerSettings);
+builder.Services.AddSingleton(Options.Create(identityServerSettings));
+
+
+#if !DEBUG
+     identityServerSettings.AccessTokenSecret = Environment.GetEnvironmentVariable("JWT_ACCESS_TOKEN_SECRET");
+#endif
+
+builder.Services.AddAuthentication().AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, x =>
+{
+    x.SaveToken = true;
+    x.RequireHttpsMetadata = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(identityServerSettings.AccessTokenSecret)),
+        ValidateAudience = false,
+        ValidateIssuer = false
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("author", p =>
+    {
+        p.RequireAuthenticatedUser();
+    });
+});
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
@@ -13,7 +51,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapReverseProxy();
 
---
+++ Settings/IdentityServerSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace GateWay.API.Settings
+{
+    public class IdentityServerSettings
+    {
+        public string Authority { get; set; }
+        public string AccessTokenSecret { get; set; }
+    }
+}
