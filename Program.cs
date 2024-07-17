bb092b04f176b1d61112403d4288c87aa15ddec8
VS\shabala <artemizgame@gmail.com>
2024-07-17 11:53:43 +1000

    Add SSL

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Certes;
+using FluffySpoon.AspNet.EncryptWeMust;
+using FluffySpoon.AspNet.EncryptWeMust.Certes;
 using GateWay.API.Settings;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -14,9 +17,24 @@ var identityServerSettings = new IdentityServerSettings();
 builder.Configuration.GetSection("IdentityServerSettings").Bind(identityServerSettings);
 builder.Services.AddSingleton(Options.Create(identityServerSettings));
 
-#if !DEBUG
-builder.Services.AddLettuceEncrypt();
-#endif
+builder.Services.AddFluffySpoonLetsEncrypt(new LetsEncryptOptions()
+{
+    Email = "artemizgame@gmail.com", //LetsEncrypt will send you an e-mail here when the certificate is about to expire
+    UseStaging = false, //switch to true for testing
+    Domains = new[] { "api.artemizgame.ru" },
+    TimeUntilExpiryBeforeRenewal = TimeSpan.FromDays(30), //renew automatically 30 days before expiry
+    TimeAfterIssueDateBeforeRenewal = TimeSpan.FromDays(7), //renew automatically 7 days after the last certificate was issued
+    CertificateSigningRequest = new CsrInfo() //these are your certificate details
+    {
+        CountryName = "Russia",
+        Locality = "RU",
+        Organization = "Artemizgame",
+        OrganizationUnit = "Hat department",
+        State = "RU"
+    }
+});
+
+builder.Services.AddFluffySpoonLetsEncryptFileCertificatePersistence();
 
 #if !DEBUG
      identityServerSettings.AccessTokenSecret = Environment.GetEnvironmentVariable("JWT_ACCESS_TOKEN_SECRET");
@@ -54,6 +72,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseFluffySpoonLetsEncrypt();
 app.UseAuthentication();
 app.UseAuthorization();
 
